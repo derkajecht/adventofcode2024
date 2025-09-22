79636ec681d295e6c96afda40e8da9586e835bf1
Jordan Mackenzie <jordantwiddy4@gmail.com>
2025-09-22 17:35:28 +0100

    Day 01

---
+++ day_01/day_01.py
@@ -0,0 +1,8 @@
+# Take two lists as separate inputs, sort them in ASCENDING order and pair those numbers with the corresponding number from the other list. Find the difference of each pair for every entry in the list and total that up. 
+
+# Get the puzzle input from input.txt and assign it to the data variable.
+with open("input.txt") as puzzleinput:
+    raw_data = puzzleinput.read()
+
+
+
